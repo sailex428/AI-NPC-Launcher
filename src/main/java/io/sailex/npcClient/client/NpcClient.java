66e10b89411d77d5955af4d9de12f456718d80a8
sailex <elias.hahn@mail.de>
2024-10-08 12:41:09 +0200

    Initial commit

---
+++ src/main/java/io/sailex/npcClient/client/NpcClient.java
@@ -0,0 +1,10 @@
+package io.sailex.npcClient.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NpcClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
