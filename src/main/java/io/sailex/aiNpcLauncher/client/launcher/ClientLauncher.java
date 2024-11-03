2fa3effd906f4f3e2832b56e1c9035fcd9167126
sailex <elias.hahn@mail.de>
2024-11-03 18:30:17 +0100

    added logic to download requested vanilla and fabric versions in subdir of minecraft client dir

--- src/main/java/io/sailex/aiNpcLauncher/client/launcher/ClientLauncher.java
+++ src/main/java/io/sailex/aiNpcLauncher/client/launcher/ClientLauncher.java
@@ -1,19 +1,23 @@
 package io.sailex.aiNpcLauncher.client.launcher;
 
+import java.nio.file.Path;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-
+import me.earth.headlessmc.api.command.CommandException;
 import me.earth.headlessmc.api.exit.ExitManager;
 import me.earth.headlessmc.launcher.Launcher;
 import me.earth.headlessmc.launcher.LauncherBuilder;
-import me.earth.headlessmc.launcher.LauncherProperties;
 import me.earth.headlessmc.launcher.auth.AuthException;
 import me.earth.headlessmc.launcher.auth.LaunchAccount;
 import me.earth.headlessmc.launcher.auth.ValidatedAccount;
+import me.earth.headlessmc.launcher.command.FabricCommand;
+import me.earth.headlessmc.launcher.command.download.DownloadCommand;
+import me.earth.headlessmc.launcher.command.download.VersionInfo;
 import me.earth.headlessmc.launcher.files.FileManager;
 import me.earth.headlessmc.launcher.launch.LaunchOptions;
 import me.earth.headlessmc.launcher.version.Version;
+import me.earth.headlessmc.launcher.version.VersionImpl;
 import net.lenni0451.commons.httpclient.HttpClient;
 import net.minecraft.SharedConstants;
 import net.minecraft.client.MinecraftClient;
@@ -35,33 +39,30 @@ public class ClientLauncher {
 		this.npcClientProcesses = npcClientProcesses;
 		this.launcher = initLauncher();
 		this.client = client;
+		setMcDir();
 	}
 
 	public void launch(String npcName, String llmType, String llmModel, boolean isOffline) {
 		CompletableFuture.runAsync(() -> {
 			try {
-				LaunchAccount account = getAccount(isOffline);
+				String versionName = SharedConstants.getGameVersion().getName();
+				Version version = findOrDownloadFabric(versionName);
 
+				LaunchAccount account = getAccount(isOffline);
 				if (account == null) {
 					logResult("Failed to login.");
 				}
 
-				String versionName = SharedConstants.getGameVersion().getName();
-				Version baseVersion = getVersion(versionName);
-
-				UUID uuid = UUID.fromString(launcher.getConfig()
-						.get(
-								LauncherProperties.EXTRACTED_FILE_CACHE_UUID,
-								UUID.randomUUID().toString()));
-				FileManager files = launcher.getFileManager().createRelative(uuid.toString());
+				FileManager files = launcher.getFileManager()
+						.createRelative(UUID.randomUUID().toString());
 
 				LaunchOptions options = LaunchOptions.builder()
 						.account(account)
-						.additionalJvmArgs(List.of("-Dllm.type=" + llmType,
+						.additionalJvmArgs(List.of(
+								"-Dllm.type=" + llmType,
 								"-Dllm.model=" + llmModel,
-								isOffline ? "-Dhmc.offline.username=" + npcName : ""
-						))
-						.version(baseVersion)
+								isOffline ? "-Dhmc.offline.username=" + npcName : ""))
+						.version(version)
 						.launcher(launcher)
 						.files(files)
 						.parseFlags(launcher, false)
@@ -86,30 +87,6 @@ public class ClientLauncher {
 		});
 	}
 
-	private LaunchAccount getAccount(boolean isOffline) throws Exception {
-		if (isOffline) {
-			return launcher.getAccountManager().getOfflineAccount(launcher.getConfig());
-		} else {
-			HttpClient httpClient = MinecraftAuth.createHttpClient();
-			StepFullJavaSession.FullJavaSession javaSession = MinecraftAuth.JAVA_DEVICE_CODE_LOGIN.getFromInput(
-					httpClient, new StepMsaDeviceCode.MsaDeviceCodeCallback(msaDeviceCode -> client.inGameHud
-							.getChatHud()
-							.addMessage(Text.of("Go to " + msaDeviceCode.getDirectVerificationUri()))));
-			ValidatedAccount validatedAccount = new ValidatedAccount(
-					javaSession, javaSession.getMcProfile().getMcToken().getAccessToken());
-			return validatedAccount.toLaunchAccount();
-		}
-	}
-
-	private Version getVersion(String versionName) {
-		for (Version version : launcher.getVersionService().getContents()) {
-			if (version.getName().contains(versionName) && version.getName().contains("fabric")) {
-				return version;
-			}
-		}
-		return null;
-	}
-
 	private Launcher initLauncher() {
 		LauncherBuilder builder = new LauncherBuilder();
 		builder.exitManager(new ExitManager());
@@ -122,9 +99,71 @@ public class ClientLauncher {
 		}
 	}
 
+	private void setMcDir() {
+		String defaultMcDir =
+				launcher.getLauncherConfig().getMcFiles().getBase().getPath();
+		FileManager fileManager =
+				new FileManager(Path.of(defaultMcDir, "ai-npc").toString());
+
+		launcher.getLauncherConfig().setMcFiles(fileManager);
+		launcher.getLauncherConfig().setGameDir(fileManager);
+	}
+
+	private Version findOrDownloadFabric(String versionName) throws CommandException {
+		Version version = getVersion(versionName);
+		if (version != null) {
+			return version;
+		}
+		Version neededVersion = VersionImpl.builder().name(versionName).build();
+
+		DownloadCommand downloadCommand = new DownloadCommand(launcher);
+		// cache version infos
+		downloadCommand.execute(versionName);
+		// get the needed version info
+		VersionInfo versionInfo = null;
+		for (VersionInfo info : downloadCommand.getIterable()) {
+			if (info.getName().equals(versionName)) {
+				versionInfo = info;
+				break;
+			}
+		}
+		// download vanilla
+		downloadCommand.execute(versionInfo);
+
+		// download fabric
+		FabricCommand fabricCommand = new FabricCommand(launcher);
+		fabricCommand.execute(neededVersion);
+
+		return getVersion(versionName);
+	}
+
+	private Version getVersion(String versionName) {
+		for (Version version : launcher.getVersionService().getContents()) {
+			if (version.getName().contains(versionName) && version.getName().contains("fabric")) {
+				return version;
+			}
+		}
+		return null;
+	}
+
+	private LaunchAccount getAccount(boolean isOffline) throws Exception {
+		if (isOffline) {
+			return launcher.getAccountManager().getOfflineAccount(launcher.getConfig());
+		} else {
+			HttpClient httpClient = MinecraftAuth.createHttpClient();
+			StepFullJavaSession.FullJavaSession javaSession = MinecraftAuth.JAVA_DEVICE_CODE_LOGIN.getFromInput(
+					httpClient, new StepMsaDeviceCode.MsaDeviceCodeCallback(msaDeviceCode -> {
+						client.inGameHud.getChatHud().addMessage(Text.of("Go to"));
+						client.inGameHud.getChatHud().addMessage(Text.of(msaDeviceCode.getDirectVerificationUri()));
+					}));
+			ValidatedAccount validatedAccount = new ValidatedAccount(
+					javaSession, javaSession.getMcProfile().getMcToken().getAccessToken());
+			return validatedAccount.toLaunchAccount();
+		}
+	}
+
 	private void logResult(String result) {
 		LOGGER.info(result);
 		client.inGameHud.getChatHud().addMessage(Text.of(result));
 	}
-
 }
